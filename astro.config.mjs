038408030e0c28b48986b7f1cd91f8af860f78a4
MAkbarD <mad4869@gmail.com>
2024-05-28 18:02:16 +0700

    Deployed 1.0.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://mad4869.github.io",
   integrations: [react(), tailwind({ applyBaseStyles: false })]
 });
